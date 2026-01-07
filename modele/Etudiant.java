2c073062d50f16f0cef9b58c73b52a150308f68e
WINDOWS\simo <md@grylab.com>
2026-01-07 23:03:29 +0100

    added getters, setters, constructors, and toString

--- modele/Etudiant.java
+++ modele/Etudiant.java
@@ -12,4 +12,70 @@ public class Etudiant {
 	private String matricule;
 	private String email;
 	
+	// Constructors
+	
+	// minimum constructor
+	public Etudiant(String nomComplet, String filiere, String niveau) {
+		this.nomComplet = nomComplet;
+        this.filiere = filiere;
+        this.niveau = niveau;
+	}
+	
+	//Complete constructor
+	public Etudiant(int id, String nomComplet, String filiere, String niveau, String matricule, String email) {
+		super();
+		this.id = id;
+		this.nomComplet = nomComplet;
+		this.filiere = filiere;
+		this.niveau = niveau;
+		this.matricule = matricule;
+		this.email = email;
+	}
+	
+	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomComplet() {
+		return nomComplet;
+	}
+	public void setNomComplet(String nomComplet) {
+		this.nomComplet = nomComplet;
+	}
+	public String getFiliere() {
+		return filiere;
+	}
+	public void setFiliere(String filiere) {
+		this.filiere = filiere;
+	}
+	public String getNiveau() {
+		return niveau;
+	}
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+
+	
+	@Override
+    public String toString() {
+        return "Etudiant [id=" + id + ", nom=" + nomComplet + ", matricule=" + (matricule != null ? matricule : "N/A") + "]";
+    }
+	
 }
