976d846d1cb7f31a8c2da74da984be63c389eae7
WINDOWS\simo <md@grylab.com>
2026-01-07 22:39:05 +0100

    added attributs

--- modele/Etudiant.java
+++ modele/Etudiant.java
@@ -1,5 +1,13 @@
 package modele;
 
 public class Etudiant {
-
+	
+	// Attributs
+	private int id; // Ajouté pour la base de données
+	private String nomComplet;
+	private String filiere;
+	private String niveau;
+	
+	
+	
 }
