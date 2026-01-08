1340efe37dfbe727ceab02c48703a18431661e78
WINDOWS\simo <md@grylab.com>
2026-01-08 04:33:11 +0100

    added the logic to retreive infos of prof from database

--- dao/ProfesseurDAO.java
+++ dao/ProfesseurDAO.java
@@ -31,4 +31,29 @@ public class ProfesseurDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	// Retrouver un prof par son nom 
+	public Professeur trouverParNom(String nomRecherche) {
+		Connection cnx = Connexion.getConnexion();
+		Professeur prof = null;
+		
+		try {
+			String sql = "SELECT * FROM professeur WHERE nom_complet = ?";
+			PreparedStatement ps = cnx.prepareStatement(sql);
+			ps.setString(1, nomRecherche);
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				// On a trouvé le prof !
+				prof = new Professeur(rs.getString("nom_complet"), rs.getString("specialite"));
+				prof.setId(rs.getInt("id")); // Très important de récupérer son ID
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return prof; // Retourne l'objet Prof ou null si pas trouvé
+	}
+	
 }
