2559559bbf224afac92fe13b5130573ea95d8990
WINDOWS\simo <md@grylab.com>
2026-01-08 01:20:52 +0100

    REMOVED MARKING management from this classe to be implemeted in examen class

--- modele/Question.java
+++ modele/Question.java
@@ -8,11 +8,7 @@ public class Question {
 	private String enonce;
 	private ArrayList<String> choix;
 	private ArrayList<Integer> bonnesReponses; //list des reponses correcte
-	
-	private double pointSiJuste; // si juste ch7al ghadi yzidlk
-	private double pointSiFaux;  // si faux ch7al ghadi yn9aslk
-	private double pointSiVide;  // ida majawbtichi cheno ghayw9a3
-	
+
 	private String media; // Chemin vers image/video
 	
 	// Constructeurs de creation
@@ -20,25 +16,15 @@ public class Question {
 		this.enonce = enonce;
 		this.choix = new ArrayList<>(); 
 		this.bonnesReponses = new ArrayList<>();
-		
-		// les notes par defaut
-		this.pointSiJuste = 1.0;
-		this.pointSiFaux = 0.0;
-		this.pointSiVide = 0.0;
 	}
 	
 	// Constructeur complet
-	public Question(int id, String enonce, double pointSiJuste, double pointSiFaux, double pointSiVide, String media) {
+	public Question(int id, String enonce, String media) {
 		this.id = id;
 		this.enonce = enonce;
-		this.pointSiJuste = pointSiJuste;
-		this.pointSiFaux = pointSiFaux;
-		this.pointSiVide = pointSiVide;
 		this.media = media;
-		
 		this.choix = new ArrayList<>();
 		this.bonnesReponses = new ArrayList<>();
-		
 	}
 	
 	
@@ -77,30 +63,6 @@ public class Question {
 		this.bonnesReponses = bonnesReponses;
 	}
 
-	public double getPointSiJuste() {
-		return pointSiJuste;
-	}
-
-	public void setPointSiJuste(double pointSiJuste) {
-		this.pointSiJuste = pointSiJuste;
-	}
-
-	public double getPointSiFaux() {
-		return pointSiFaux;
-	}
-
-	public void setPointSiFaux(double pointSiFaux) {
-		this.pointSiFaux = pointSiFaux;
-	}
-
-	public double getPointSiVide() {
-		return pointSiVide;
-	}
-
-	public void setPointSiVide(double pointSiVide) {
-		this.pointSiVide = pointSiVide;
-	}
-
 	public String getMedia() {
 		return media;
 	}
@@ -113,10 +75,7 @@ public class Question {
 	
 	@Override
 	public String toString() {
-		return "Question: " + enonce + 
-			   " [Barème: Juste=" + pointSiJuste + ", Faux=" + pointSiFaux + ", Vide=" + pointSiVide + "]" +
-			   "\nChoix=" + choix + 
-			   "\nCorrects=" + bonnesReponses;
+		return "Question [id=" + id + ", enonce=" + enonce + ", choix=" + choix + "]";
 	}
 	
 	
