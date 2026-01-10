ee46a815044f1e7bc6556a68369fe70e033bcaec
WINDOWS\simo <md@grylab.com>
2026-01-10 02:18:55 +0100

    made Getter and setters more visible (no change in code)

--- modele/Question.java
+++ modele/Question.java
@@ -80,19 +80,50 @@ public class Question {
 	}
 	
 	// Getters and Setters
-	public int getId() { return id; }
-	public void setId(int id) { this.id = id; }
-	public String getEnonce() { return enonce; }
-	public void setEnonce(String enonce) { this.enonce = enonce; }
-	public ArrayList<String> getChoix() { return choix; }
-	public void setChoix(ArrayList<String> choix) { this.choix = choix; }
-	public ArrayList<Integer> getBonnesReponses() { return bonnesReponses; }
-	public void setBonnesReponses(ArrayList<Integer> bonnesReponses) { this.bonnesReponses = bonnesReponses; }
-	public String getMedia() { return media; }
-	public void setMedia(String media) { this.media = media; }
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEnonce() {
+		return enonce;
+	}
+
+	public void setEnonce(String enonce) {
+		this.enonce = enonce;
+	}
+
+	public ArrayList<String> getChoix() {
+		return choix;
+	}
+
+	public void setChoix(ArrayList<String> choix) {
+		this.choix = choix;
+	}
+
+	public ArrayList<Integer> getBonnesReponses() {
+		return bonnesReponses;
+	}
+
+	public void setBonnesReponses(ArrayList<Integer> bonnesReponses) {
+		this.bonnesReponses = bonnesReponses;
+	}
+
+	public String getMedia() {
+		return media;
+	}
+
+	public void setMedia(String media) {
+		this.media = media;
+	}
 	
 	@Override
 	public String toString() {
 		return "Question [id=" + id + ", enonce=" + enonce + "]";
 	}
+
+
 }
