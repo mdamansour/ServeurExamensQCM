0752360bbd8d565a1c4daeac10c9ee447ea6bc92
WINDOWS\simo <md@grylab.com>
2026-01-11 00:37:33 +0100

    the same file but with more visible comments

--- gui/AuthentificationGUI.java
+++ gui/AuthentificationGUI.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -13,19 +14,21 @@ public class AuthentificationGUI extends JFrame {
 	
 	
 	public AuthentificationGUI () {
-		
-		
-		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setTitle("This is the title of the window");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 		
-		
-		
+		//elements for screen dimesions
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int x = screenSize.width;
 		int y = screenSize.height;
-
-		this.setTitle("This is the title of the window");
 		this.setSize(x/2, y/2);		//	determine the dimensions of the window in function of width and hight
-		this.setLocationRelativeTo(null);
+		this.setLocationRelativeTo(null);	//Set the position of the window relative to nothing (center)
+		
+		this.setLayout(new BorderLayout());
+		
+		
+		// make the window visible
 		this.setVisible(true);
 	}
 	
